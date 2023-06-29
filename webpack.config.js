b77396402434e9b7d0e777adf41ff020094333bd
Scott Wright <scott.wright.uk@gmail.com>
2023-06-29 12:27:43 +0100

    add: webpack dependencies
    This commit adds additional webpack dependencies including css and image loaders as well as webpack sever to view changes to the project without having to reload.
    The index.html has additionally changed the script file name to bundle.js from main.js to reflect thats the final js file will be a bundle as per best practices.

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,29 @@ module.exports = {
     mode: 'development', 
     entry: './src/index.js',
     output: {
-        filename: 'main.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist')
+        },   
+        port: 3000,  
+        open: true,
+        hot: true, 
+        compress: true,
+        historyApiFallback: true,    
+    },
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+          },
+          {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
+          },    
+        ],
+      },
 };
