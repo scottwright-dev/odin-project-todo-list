4d9c11c5256c595388eae2cfc8e989514d5c6ccc
Scott Wright <scott.wright.uk@gmail.com>
2023-07-04 18:43:00 +0100

    refactor: form creation in renderToDoTask
    This commit refactors the code for creating the form in the renderToDoTask() function.
    
    While focusing on a modular approach my previous code had considerable repetition. Upon reflection the following changes have been implemented in this commit.
    
    The refactoring introduces two helper functions: createInputLabel() and createPriorityOption().
    These functions encapsulate the creation of the label/input elements and the option elements, reducing code duplication.
    
    I contemplated modifying the createInputLabel function to also have the ability to create the submit button but feel this would be blurring the lines of what the helper function is created to do.

--- src/modules/domRendering.js
+++ src/modules/domRendering.js
@@ -1,70 +1,60 @@
 import createToDoTask from './createToDoTask';
 
 export function renderToDoTask() {
+  const renderContent = document.querySelector('.render-content');
+  renderContent.innerHTML = "";
 
-    const renderContent = document.querySelector('.render-content');
-    renderContent.innerHTML = "";
+  const formContainer = document.createElement('div');
+  formContainer.classList.add('todo-form-container');
 
-    const formContainer = document.createElement('div');
-    formContainer.classList.add('todo-form-container');
-    
-    const form = document.createElement('form');
-    form.classList.add('todo-form');
-    formContainer.appendChild(form);
+  const form = document.createElement('form');
+  form.classList.add('todo-form');
+  formContainer.appendChild(form);
 
-    const titleLabel = document.createElement('label');
-    titleLabel.textContent = 'Title';
-    const titleInput = document.createElement('input');
-    titleInput.type = 'text';
-    titleLabel.classList.add('task-title-input');
-    titleLabel.appendChild(titleInput);
-    form.appendChild(titleLabel);
+  function createInputLabel(text, inputType, inputClass) {
+    const label = document.createElement('label');
+    label.textContent = text;
+    const input = document.createElement('input');
+    input.type = inputType;
+    input.classList.add(inputClass);
+    label.appendChild(input);
+    return label;
+  }
 
-    const descriptionLabel = document.createElement('label');
-    descriptionLabel.textContent = 'Description';
-    const descriptionInput = document.createElement('input');
-    descriptionInput.type = 'text';
-    descriptionInput.classList.add('task-description-input');
-    descriptionLabel.appendChild(descriptionInput);
-    form.appendChild(descriptionLabel);    
+  const titleLabel = createInputLabel('Title:', 'text', 'task-title-input');
+  form.appendChild(titleLabel);
 
-    const dueDateLabel = document.createElement('label');
-    dueDateLabel.textContent = 'Date';
-    const dueDateInput = document.createElement('input');
-    dueDateInput.type = 'date';
-    dueDateInput.classList.add('task-dueDate-input');
-    dueDateLabel.appendChild(dueDateInput);
-    form.appendChild(dueDateLabel);
+  const descriptionLabel = createInputLabel('Description:', 'text', 'task-description-input');
+  form.appendChild(descriptionLabel);
 
-    const priorityLabel = document.createElement('label');
-    priorityLabel.textContent = 'Priority';
-    const prioritySelect = document.createElement('select');
-    prioritySelect.classList.add('task-priority-select');
-    priorityLabel.appendChild(prioritySelect);
-    form.appendChild(priorityLabel);
+  const dueDateLabel = createInputLabel('Date:', 'date', 'task-dueDate-input');
+  form.appendChild(dueDateLabel);
 
-    const lowPriority = document.createElement('option');
-    lowPriority.value = 'low';
-    lowPriority.textContent = 'Low - !';
-    prioritySelect.appendChild(lowPriority);
+  function createPriorityOption(value, text) {
+    const option = document.createElement('option');
+    option.value = value;
+    option.textContent = text;
+    return option;
+  }
 
-    const medPriority = document.createElement('option');
-    medPriority.value = 'medium';
-    medPriority.textContent = 'Medium - !!';
-    prioritySelect.appendChild(medPriority);
+  const priorityLabel = createInputLabel('Priority:', 'select', 'task-priority-select');
+  const prioritySelect = priorityLabel.querySelector('select');
+  form.appendChild(priorityLabel);
 
-    const highPriority = document.createElement('option');
-    highPriority.value = 'high';
-    highPriority.textContent = 'High - !!!';
-    prioritySelect.appendChild(highPriority);
+  const lowPriority = createPriorityOption('low', 'Low - !');
+  prioritySelect.appendChild(lowPriority);
 
-    // need to figure out adding to a list
+  const medPriority = createPriorityOption('medium', 'Medium - !!');
+  prioritySelect.appendChild(medPriority);
 
-    const submitBtn = document.createElement('input');
-    submitBtn.type = 'submit';
-    submitBtn.value = 'add task';
-    submitBtn.classList.add('task-submit-btn');
-    form.appendChild(submitBtn);
+  const highPriority = createPriorityOption('high', 'High - !!!');
+  prioritySelect.appendChild(highPriority);
 
-    renderContent.appendChild(formContainer);
+  const submitBtn = document.createElement('input');
+  submitBtn.type = 'submit';
+  submitBtn.value = 'add task';
+  submitBtn.classList.add('task-submit-btn');
+  form.appendChild(submitBtn);
+
+  renderContent.appendChild(formContainer);
 }
