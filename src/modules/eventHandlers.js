e9e47adc557ff3e7d785513cbecc1bfe00253fc3
Scott Wright <scott.wright.uk@gmail.com>
2023-07-05 15:50:12 +0100

    add: addTaskToList function
    This commit adds the addTaskToList function which is responsible for handling the to do task form submission and adding a new task to a list.
    
    The plan is to get the basic functionality working so we can display the tasks on screen in a default list.
    Additional functionality will be added later, such as for editing task data and moving created tasks between lists.
    I am trying to keep a reasonable level of atomic git commits to the project to build it incrementally and logically.

--- src/modules/eventHandlers.js
+++ src/modules/eventHandlers.js
@@ -1,7 +1,36 @@
-import { renderToDoTask } from "./domRendering";
+import { renderToDoTask } from './domRendering';
+import createToDoTask from './createToDoTask';
+import { createList } from './listManager';
 
-const addTaskButton = document.querySelector(".add-task-btn");
+export function addTaskToList() {
+  const form = document.querySelector('.todo-form');
+  const taskList = createList();
 
-addTaskButton.addEventListener("click", () => {
-  renderToDoTask();
-});
+  if (form) {
+    form.addEventListener('submit', (event) => {
+      event.preventDefault();
+
+      const title = document.querySelector('.task-title-input').value;
+      const description = document.querySelector('.task-description-input').value;
+      const dueDate = document.querySelector('.task-dueDate-input').value;
+      const priority = document.querySelector('.task-priority-select').value;
+
+      const addTask = createToDoTask(title, description, dueDate, priority);
+      taskList.addTaskToList(addTask);
+
+      console.log(taskList.taskList); 
+
+      form.reset();
+    });
+  }
+}
+
+export function handleAddTaskButtonClick() {
+    const addTaskButton = document.querySelector(".add-task-btn");
+  
+    addTaskButton.addEventListener("click", () => {
+      renderToDoTask();
+      addTaskToList();
+    });
+  }
+  
