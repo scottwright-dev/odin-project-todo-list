d48a579153b143adebe9c522ae67277dda61fcff
Scott Wright <scott.wright.uk@gmail.com>
2023-07-07 12:29:51 +0100

    refactor: add complete property & amend error log
    This commit adds a new complete property which will store the state of each task. True = complete. False , the default = incomplete.
    
    Additional changes:
    - Change the error log for when the title i s empty to be an alert instead, this allows for faster development/testing.
    - refactor the return statement for improved readability.

--- src/modules/createToDoTask.js
+++ src/modules/createToDoTask.js
@@ -1,7 +1,14 @@
 export default function createToDoTask(task, description, dueDate, priority, list) {
   if (!task) {
-    throw new Error('Please add a task');
+    alert ('Task cannot be empty, please add task name');
   }
 
-  return { task, description, dueDate, priority, list }
+  return { 
+    task, 
+    description, 
+    dueDate, 
+    priority, 
+    list,
+    complete: false,
+   }
 }
