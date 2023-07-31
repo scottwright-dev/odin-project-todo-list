f205f2b8414c87003292c3d91228c6bee18f4065
Scott Wright <scott.wright.uk@gmail.com>
2023-07-31 09:46:38 +0100

    feat: add date formatting via date-fns library
    This commit adds the date formatting function from the date-fns utility library.
    As I am based in the UK the standard date format style is dd/mm/yyyy which has been reflected in the function to format the date rather than the standard js format of: yyyy-mm-dd.
    
    This makes the date format more readable an understandable.

--- src/modules/createToDoTask.js
+++ src/modules/createToDoTask.js
@@ -1,14 +1,23 @@
-export default function createToDoTask(task, description, dueDate, priority, list) {
-  if (!task) {
-    alert ('Task cannot be empty, please add task name');
+import { format } from 'date-fns';
+
+  function formatDate(date) {
+    const formattedDate = format(new Date(date), 'dd/MM/yyyy');
+    return formattedDate;
   }
 
-  return { 
-    task, 
-    description, 
-    dueDate, 
-    priority, 
+export default function createToDoTask(task, description, dueDate, priority, list) {
+  if (!task) {
+    alert('Task cannot be empty, please add task name');
+  }
+
+  const formattedDueDate = formatDate(dueDate);
+
+  return {
+    task,
+    description,
+    dueDate: formattedDueDate,
+    priority,
     list,
     complete: false,
-   }
+  };
 }
